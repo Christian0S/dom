6a7a181d6a713af70573b6fd3d27ec7b73904c5a
Christian0S <lx08055@innovaccion.mx>
2023-03-02 18:03:41 -0500

    clase 2

--- scrip.js
+++ scrip.js
@@ -3,7 +3,17 @@ const btn = document.querySelector("[data-from-btn]");
 const createTask = (evento) => {
     evento.preventDefault();
     let input = document.querySelector("[data-from-input]");
-    console.log(input.value);
+    const value = input.value;
+    const task = document.querySelector("[data-task]");
+    input.value = '';
+    const content = ` <div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`
+    task.innerHTML = content;
+    console.log(task);
+
 };
 
 btn.addEventListener("click", createTask);
